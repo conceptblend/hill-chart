7b5d5784468c33f826cda491d881a9c0246823c2
conceptblend <the.concept.blend@gmail.com>
2024-03-03 21:32:13 -0700

    tmp: another test endpoint

--- src/index.js
+++ src/index.js
@@ -58,12 +58,6 @@ router
       showLabels: !!!q.hideLabels,
     });
   })
-  /**
-   * @typedef {Object} hillChartPublicOptions
-   * @property {string} [title] - The title for the image
-   * @property {boolean} [hideLabels=false] - Hide the labels
-   * @property {any} [download=false] - Immediately download the image. Any value triggers it
-   */
   .get("/vnext/:t/:imageType?", (ctx, next) => {
     // ctx.router available
     //
@@ -102,6 +96,36 @@ router
     //   "Content-Disposition",
     //   `${dispositionType}; filename="${dispositionFilename}"`,
     // );
+    // ctx.set("X-Content-Type-Options", "nosniff");
+    ctx.set("Cache-Control", "public, max-age:86400");
+    ctx.body = streamHillChart(t, imageType, {
+      title: q.title || "",
+      showLabels: !!!q.hideLabels,
+    });
+  })
+  .get("/vnext/:t/hill-chart.png", (ctx, next) => {
+    // ctx.router available
+    //
+    const q = ctx.request.query;
+    const tRaw = parseInt(ctx.params.t);
+    const imageType = "png";
+    // Restrict `t` to Numbers
+    if (typeof tRaw !== "number" || Number.isNaN(tRaw)) {
+      ctx.throw(422, "Parameter `t` must be a number");
+    }
+    // Clamp between 0..100 then map to 0..1
+    const tClamped = Math.min(100, Math.max(0, tRaw));
+    const t = tClamped / 100;
+
+    const dispositionType = q.download ? "attachment" : "inline";
+    const cleanTitle = q.title ? "-" + q.title.replace(/\s/g, "_") : "";
+
+    ctx.type = "image/png";
+    const dispositionFilename = `hill-chart-at-${tClamped}${cleanTitle}.${imageType}`;
+    ctx.set(
+      "Content-Disposition",
+      `${dispositionType}; filename="${dispositionFilename}"`,
+    );
     // ctx.set("X-Content-Type-Options", "nosniff");
     ctx.set("Cache-Control", "public, max-age:86400");
     ctx.body = streamHillChart(t, imageType, {
