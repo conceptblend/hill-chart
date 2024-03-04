4adaa970972190422da4e9b6465c3394ed3ff21e
conceptblend <the.concept.blend@gmail.com>
2024-03-03 21:10:25 -0700

    tmp: add alt endpoint for testing

--- src/index.js
+++ src/index.js
@@ -12,51 +12,103 @@ const PORT = process.env.PORT || 3000;
  * @property {boolean} [hideLabels=false] - Hide the labels
  * @property {any} [download=false] - Immediately download the image. Any value triggers it
  */
-router.get("/:t/:imageType?", (ctx, next) => {
-  // ctx.router available
-  //
-  const q = ctx.request.query;
-  const tRaw = parseInt(ctx.params.t);
-  const imageType = ctx.params.imageType
-    ? ctx.params.imageType.toLowerCase()
-    : "jpg";
-  // Restrict `t` to Numbers
-  if (typeof tRaw !== "number" || Number.isNaN(tRaw)) {
-    ctx.throw(422, "Parameter `t` must be a number");
-  }
-  // Clamp between 0..100 then map to 0..1
-  const tClamped = Math.min(100, Math.max(0, tRaw));
-  const t = tClamped / 100;
+router
+  .get("/:t/:imageType?", (ctx, next) => {
+    // ctx.router available
+    //
+    const q = ctx.request.query;
+    const tRaw = parseInt(ctx.params.t);
+    const imageType = ctx.params.imageType
+      ? ctx.params.imageType.toLowerCase()
+      : "jpg";
+    // Restrict `t` to Numbers
+    if (typeof tRaw !== "number" || Number.isNaN(tRaw)) {
+      ctx.throw(422, "Parameter `t` must be a number");
+    }
+    // Clamp between 0..100 then map to 0..1
+    const tClamped = Math.min(100, Math.max(0, tRaw));
+    const t = tClamped / 100;
 
-  const dispositionType = q.download ? "attachment" : "inline";
-  const cleanTitle = q.title ? "-" + q.title.replace(/\s/g, "_") : "";
-  let mimeType = "";
+    const dispositionType = q.download ? "attachment" : "inline";
+    const cleanTitle = q.title ? "-" + q.title.replace(/\s/g, "_") : "";
+    let mimeType = "";
 
-  switch (imageType) {
-    case "png":
-      mimeType = "image/png";
-      break;
-    case "svg":
-      mimeType = "image/svg+xml";
-      break;
-    default:
-      mimeType = "image/jpeg";
-      break;
-  }
+    switch (imageType) {
+      case "png":
+        mimeType = "image/png";
+        break;
+      case "svg":
+        mimeType = "image/svg+xml";
+        break;
+      default:
+        mimeType = "image/jpeg";
+        break;
+    }
 
-  ctx.type = mimeType;
-  const dispositionFilename = `hill-chart-at-${tClamped}${cleanTitle}.${imageType}`;
-  ctx.set(
-    "Content-Disposition",
-    `${dispositionType}; filename="${dispositionFilename}"`,
-  );
-  // ctx.set("X-Content-Type-Options", "nosniff");
-  ctx.set("Cache-Control", "public, max-age:86400");
-  ctx.body = streamHillChart(t, imageType, {
-    title: q.title || "",
-    showLabels: !!!q.hideLabels,
+    ctx.type = mimeType;
+    const dispositionFilename = `hill-chart-at-${tClamped}${cleanTitle}.${imageType}`;
+    ctx.set(
+      "Content-Disposition",
+      `${dispositionType}; filename="${dispositionFilename}"`,
+    );
+    // ctx.set("X-Content-Type-Options", "nosniff");
+    ctx.set("Cache-Control", "public, max-age:86400");
+    ctx.body = streamHillChart(t, imageType, {
+      title: q.title || "",
+      showLabels: !!!q.hideLabels,
+    });
+  })
+  /**
+   * @typedef {Object} hillChartPublicOptions
+   * @property {string} [title] - The title for the image
+   * @property {boolean} [hideLabels=false] - Hide the labels
+   * @property {any} [download=false] - Immediately download the image. Any value triggers it
+   */
+  .get("/vnext/:t/:imageType?", (ctx, next) => {
+    // ctx.router available
+    //
+    const q = ctx.request.query;
+    const tRaw = parseInt(ctx.params.t);
+    const imageType = ctx.params.imageType
+      ? ctx.params.imageType.toLowerCase()
+      : "jpg";
+    // Restrict `t` to Numbers
+    if (typeof tRaw !== "number" || Number.isNaN(tRaw)) {
+      ctx.throw(422, "Parameter `t` must be a number");
+    }
+    // Clamp between 0..100 then map to 0..1
+    const tClamped = Math.min(100, Math.max(0, tRaw));
+    const t = tClamped / 100;
+
+    const dispositionType = q.download ? "attachment" : "inline";
+    const cleanTitle = q.title ? "-" + q.title.replace(/\s/g, "_") : "";
+    let mimeType = "";
+
+    switch (imageType) {
+      case "png":
+        mimeType = "image/png";
+        break;
+      case "svg":
+        mimeType = "image/svg+xml";
+        break;
+      default:
+        mimeType = "image/jpeg";
+        break;
+    }
+
+    ctx.type = mimeType;
+    // const dispositionFilename = `hill-chart-at-${tClamped}${cleanTitle}.${imageType}`;
+    // ctx.set(
+    //   "Content-Disposition",
+    //   `${dispositionType}; filename="${dispositionFilename}"`,
+    // );
+    // ctx.set("X-Content-Type-Options", "nosniff");
+    ctx.set("Cache-Control", "public, max-age:86400");
+    ctx.body = streamHillChart(t, imageType, {
+      title: q.title || "",
+      showLabels: !!!q.hideLabels,
+    });
   });
-});
 
 app.use(router.routes()).use(router.allowedMethods());
 
